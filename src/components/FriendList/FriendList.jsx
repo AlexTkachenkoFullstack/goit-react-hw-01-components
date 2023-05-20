e2ec367426ddc37d6145323176932a2b2b63a9dc
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-05-20 21:15:36 +0100

    delete console.log

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import css from './FriendList.module.css'
 import { FriendListItem } from "components/FriendListItem/FriendListItem"
 export const FriendList = ({ friends }) => {
-        console.log(<FriendListItem/>)
     return (<ul className={css.friendList}>
             <FriendListItem friends={friends} />
     </ul>
